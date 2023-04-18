cdb88890ad5b9d8c02d07b1cd025f73cea19581e
BahruzAlizada <behruzelzadee@gmail.com>
2023-04-18 19:58:15 +0400

    Update and Detail SliderInfo in Admin Panel

---
+++ Fiorello/Fiorello/Areas/Admin/Controllers/SliderInfoController.cs
@@ -0,0 +1,70 @@
+﻿using Fiorello.DAL;
+using Fiorello.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Fiorello.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SliderInfoController : Controller
+    {
+        private readonly AppDbContext _db;
+        private readonly IWebHostEnvironment _env;
+
+        public SliderInfoController(AppDbContext db,IWebHostEnvironment env)
+        {
+            _env = env;
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            SliderInfo sliderInfo = await _db.SliderInfo.FirstOrDefaultAsync();
+
+            return View(sliderInfo);
+        }
+
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            SliderInfo dbsliderinfo = await _db.SliderInfo.FirstOrDefaultAsync(x => x.Id == id);
+            if (dbsliderinfo == null)
+                return BadRequest();
+
+            return View(dbsliderinfo);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Update(int? id,SliderInfo sliderInfo)
+        {
+            if (id == null)
+                return NotFound();
+            SliderInfo dbsliderinfo = await _db.SliderInfo.FirstOrDefaultAsync(x => x.Id == id);
+            if (dbsliderinfo == null)
+                return BadRequest();
+
+            dbsliderinfo.Title = sliderInfo.Title;
+            dbsliderinfo.SubTitle = sliderInfo.SubTitle;
+            dbsliderinfo.Description= sliderInfo.Description;
+
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            SliderInfo dbsliderInfo = await _db.SliderInfo.FirstOrDefaultAsync(x=>x.Id==id);
+            if (dbsliderInfo == null)
+                return BadRequest();
+
+            return View(dbsliderInfo);
+        }
+    }
+}
--- Fiorello/Fiorello/Models/SliderInfo.cs
+++ Fiorello/Fiorello/Models/SliderInfo.cs
@@ -1,8 +1,11 @@
-﻿namespace Fiorello.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fiorello.Models
 {
     public class SliderInfo
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public string SubTitle { get; set; }
         public string Description { get; set; }
